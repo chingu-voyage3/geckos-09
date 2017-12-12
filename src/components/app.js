4ce626115750ef9a09db16b0df412a9fef8b6be0
joe <joe@astucc.io>
2017-12-12 01:29:13 -0500

    Add checkin - Joe

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import JordanCheckin from './jordan';
-
+import React, { Component } from "react";
+import JordanCheckin from "./jordan";
+import JoeCheckin from "./joe";
 
 export default class App extends Component {
   render() {
     return (
-        <div className="wrapper">
-            <JordanCheckin />
-        </div>
+      <div className="wrapper">
+        <JordanCheckin />
+        <JoeCheckin />
+      </div>
     );
   }
 }
---
+++ src/components/joe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JoeCheckin = () => {
+    return (
+      <h3 className="joe" id="ninja-rockstar-lol">Joe checking in!</h3>
+    );
+  };
+
+  export default JoeCheckin;
