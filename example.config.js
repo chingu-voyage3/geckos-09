34e065969bbcda9a6bab66c1eb0fc8385c4bf1c0
Jordan Leslie <this.codedev@gmail.com>
2017-12-11 22:19:15 -0600

    Set up boilerplate. Finish check-in

---
+++ example.config.js
@@ -0,0 +1,5 @@
+// This file is for storing your API keys
+// Be sure to add this file to your .gitignore
+// Rename this file to config.js
+
+const API_KEY = 'Key here';
---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import JordanCheckin from './jordan';
+
+
+export default class App extends Component {
+  render() {
+    return (
+        <div className="wrapper">
+            <JordanCheckin />
+        </div>
+    );
+  }
+}
