0bc807df74035690440f19d75c25573d44fde4b7
Jordan Leslie <this.codedev@gmail.com>
2017-12-18 07:14:20 +0900

    Add title, info and icon

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <section className="flex-container">
+            <h4 className="header-title">Trello Lite</h4>
+            <div className="header-info">i</div>
+            <img className="header-icon img" src={user.icon}/>
+        </section>
+    );
+}
