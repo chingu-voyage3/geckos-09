b4819918f7ba3e5d30c1390dd9e4bfa169d99e9b
Jordan Leslie <this.codedev@gmail.com>
2017-12-18 13:43:22 +0900

    Add comments to header child components

--- src/components/board-menu.js
+++ src/components/board-menu.js
@@ -3,9 +3,11 @@ import React from 'react';
 export default function BoardMenu(props) {
     return (
         <div className="board-menu-container">
+            {/*The props are being passed from the redux store to header and then to BoardMenu*/}
             <h4>{props.board.name}</h4>
             <h5>{props.board.teamName}</h5>
             <div className="slide-out-menu">
+                {/*This will be a link that opens the board menu modal*/}
                 menu
             </div>
         </div>
