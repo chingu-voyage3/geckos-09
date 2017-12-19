c35e861c2b315e4ec679f21f4324c7a2e0f1f1fb
Jordan Leslie <this.codedev@gmail.com>
2017-12-19 17:53:38 +0900

    Add gecko image for about-us modal, info button opens a modal

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../containers/header';
 
-
 export default class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import ReduxPromise from 'redux-promise';
-import App from './components/app';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import ReduxPromise from "redux-promise";
+import App from "./components/app";
+import reducers from "./reducers";
+
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 const composeEnhancers =
