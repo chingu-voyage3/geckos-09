3104425b5ea6c529f3e96b6b20b7b8969108994e
Jordan Leslie <this.codedev@gmail.com>
2017-12-19 17:49:47 +0900

    Remove unused imports/exports, add comments, add menu text to board-menu

--- src/components/board-menu.js
+++ src/components/board-menu.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
+{/*The props are being passed from the redux store to header and then to BoardMenu*/
+}
 export default function BoardMenu(props) {
     return (
         <div className="board-menu-container">
-            {/*The props are being passed from the redux store to header and then to BoardMenu*/}
             <h4>{props.board.name}</h4>
             <h5>{props.board.teamName}</h5>
-            <div className="slide-out-menu">
-                {/*This will be a link that opens the board menu modal*/}
-                menu
-            </div>
+            <h5>menu</h5> {/*This will open the board menu modal*/}
         </div>
     )
 }
