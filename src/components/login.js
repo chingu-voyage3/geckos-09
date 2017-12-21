0ac80698714f9caccf3752183aa773ed91a42523
Andrew Knox <aknox0329@gmail.com>
2017-12-21 11:29:19 -0800

    commiting to show someone

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Register from './register';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
@@ -14,38 +10,35 @@ import css from '../style/login.css';
 class Login extends Component {
   render() {
     return (
-      <Router>
-        <div className="login">
-          <AppBar
-            title="Trello Lite"
-            className="header"
+      <div className="login">
+        <AppBar
+          title="Trello Lite"
+          className="header"
+        />
+        <h3 className="login-title">
+          Log in to Trello Lite
+        </h3>
+        <h5 className="register-link">
+          or <Link to="/register">Create an Account</Link>
+        </h5>
+        <form className="login-form">
+          <TextField
+            className="login-email"
+            type="email"
+            name="email"
+            floatingLabelText="email"
           />
-          <h3 className="login-title">
-            Log in to Trello Lite
-          </h3>
-          <h5 className="register-link">
-            or <Link to="/register">Create an Account</Link>
-          </h5>
-          <form className="login-form">
-            <TextField
-              className="login-email"
-              type="email"
-              name="email"
-              floatingLabelText="email"
-            />
-            <TextField
-              className="login-password"
-              type="password"
-              name="password"
-              floatingLabelText="password"
-            />
-            <RaisedButton className="login-button">
-              login
-            </RaisedButton>
-          </form>
-          <Route path="/register" component={Register} />
-        </div>
-      </Router>
+          <TextField
+            className="login-password"
+            type="password"
+            name="password"
+            floatingLabelText="password"
+          />
+          <RaisedButton className="login-button">
+            login
+          </RaisedButton>
+        </form>
+      </div>
     );
   }
 }
