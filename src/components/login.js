d1e5a8a039455d7848aada6db9b36633348f7969
Andrew Knox <aknox0329@gmail.com>
2017-12-21 10:38:48 -0800

    trying to implement a link back to the register page

--- src/components/login.js
+++ src/components/login.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+} from 'react-router-dom';
+import Register from './register';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import BoardMenu from '../components/board-menu';
-import TitleMenu from '../components/title-menu';
 import css from '../style/login.css';
 
 class Login extends Component {
   render() {
     return (
-      <div className="login">
-        <AppBar
-          title="Trello Lite"
-          className="header"
-        />
-        <form className="login-form">
-          <TextField
-            className="login-email"
-            type="email"
-            name="email"
-            floatingLabelText="email"
+      <Router>
+        <div className="login">
+          <AppBar
+            title="Trello Lite"
+            className="header"
           />
-          <TextField
-            className="login-password"
-            type="password"
-            name="password"
-            floatingLabelText="password"
-          />
-          <RaisedButton className="login-button">
-            login
-          </RaisedButton>
-        </form>
-      </div>
+          <h3 className="login-title">
+            Log in to Trello Lite
+          </h3>
+          <h5 className="register-link">
+            or <Link to="/register">Create an Account</Link>
+          </h5>
+          <form className="login-form">
+            <TextField
+              className="login-email"
+              type="email"
+              name="email"
+              floatingLabelText="email"
+            />
+            <TextField
+              className="login-password"
+              type="password"
+              name="password"
+              floatingLabelText="password"
+            />
+            <RaisedButton className="login-button">
+              login
+            </RaisedButton>
+          </form>
+          <Route path="/register" component={Register} />
+        </div>
+      </Router>
     );
   }
 }
