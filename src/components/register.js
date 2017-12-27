334f8031ad8e2e96e3831d2e1c134fc00d65b333
Andrew Knox <aknox0329@gmail.com>
2017-12-26 17:05:36 -0800

    initial firebase hookup

--- src/components/register.js
+++ src/components/register.js
@@ -1,52 +1,157 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as firebase from "firebase";
+import firebaseui from "firebaseui";
+import { ValidatorForm } from "react-form-validator-core";
+import { TextValidator } from "react-material-ui-form-validator";
 import FlatButton from "material-ui/FlatButton";
-import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 import Header from "./Header";
 import css from "../style/register.css";
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmpassword: ""
+      },
+      submitted: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { formData } = this.state;
+    formData[event.target.name] = event.target.value;
+    this.setState({ formData });
+  }
+
+  handleSubmit() {
+    this.setState({ submitted: true });
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(
+        this.state.formData.email,
+        this.state.formData.password
+      )
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  }
+
   render() {
+    const uiConfig = {
+      signInSuccessUrl: "/home",
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: ""
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start("#firebaseui-auth-container", uiConfig);
+
+    const { formData, submitted } = this.state;
     return (
       <div className="register">
-        <form className="register-form">
+        <ValidatorForm
+          className="register-form"
+          ref="form"
+          onSubmit={this.handleSubmit}
+        >
           <h3 className="welcome">Welcome!</h3>
           <h5 className="link">
             Already registered? <Link to="/login">Login</Link>
           </h5>
-          <TextField
-            className="firstname"
-            type="text"
-            name="firstname"
+          <TextValidator
             floatingLabelText="First Name"
+            onChange={this.handleChange}
+            className="firstname"
+            name="firstname"
+            value={formData.firstname}
+            validators={[
+              "required",
+              "maxStringLength:24",
+              "matchRegexp:[^0-9]$"
+            ]}
+            errorMessages={[
+              "This field is required",
+              "Names must be less than 24 characters",
+              "No numbers allowed in this field"
+            ]}
           />
-          <TextField
-            className="lastname"
-            type="text"
-            name="lastname"
+          <TextValidator
             floatingLabelText="Last Name"
+            onChange={this.handleChange}
+            className="lastname"
+            name="lastname"
+            value={formData.lastname}
+            validators={[
+              "required",
+              "maxStringLength:24",
+              "matchRegexp:[^0-9]$"
+            ]}
+            errorMessages={[
+              "This field is required",
+              "Names must be less than 24 characters",
+              "No numbers allowed in this field"
+            ]}
           />
-          <TextField
+          <TextValidator
+            floatingLabelText="email"
+            onChange={this.handleChange}
             className="email"
             type="email"
             name="email"
-            floatingLabelText="email"
+            value={formData.email}
+            validators={["required", "isEmail"]}
+            errorMessages={["This field is required", "Invalid email"]}
           />
-          <TextField
+          <TextValidator
+            floatingLabelText="password"
+            onChange={this.handleChange}
             className="password"
             type="password"
             name="password"
-            floatingLabelText="password"
+            value={formData.password}
+            validators={["required"]}
+            errorMessages={["This field is required"]}
           />
-          <TextField
+          <TextValidator
+            floatingLabelText="confirm password"
+            onChange={this.handleChange}
             className="confirmpassword"
             type="password"
             name="confirmpassword"
-            floatingLabelText="confirm password"
+            value={formData.confirmpassword}
+            validators={["required"]}
+            errorMessages={["This field is required"]}
           />
-          <RaisedButton className="register-button">Register</RaisedButton>
-        </form>
+          <RaisedButton
+            className="register-button"
+            type="submit"
+            label={submitted ? "Submitted!" : "Submit"}
+            disabled={submitted}
+          />
+          <div className="otherforms" id="firebaseui-auth-container" />
+        </ValidatorForm>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import ReduxPromise from 'redux-promise';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import App from './components/app';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import ReduxPromise from "redux-promise";
+import * as firebase from "firebase";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import firebaseConfig from "../.firebase.config";
+import App from "./components/app";
+import reducers from "./reducers";
+import css from "./style/style.css";
 
+firebase.initializeApp(firebaseConfig);
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 const composeEnhancers =
-    typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      }) : compose;
+      })
+    : compose;
 
 const enhancer = composeEnhancers(applyMiddleware(ReduxPromise));
 const store = createStore(reducers, enhancer);
@@ -25,5 +29,5 @@ ReactDOM.render(
       <App />
     </MuiThemeProvider>
   </Provider>,
-  document.querySelector('.app-container'),
+  document.querySelector(".app-container")
 );
