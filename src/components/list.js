2a52f38c124db2b499a1798b7edcb541d9c6cd01
joe <joe@astucc.io>
2017-12-30 13:12:13 -0500

    gitignored firebase.config.js

---
+++ src/components/list.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function List(props) {
+  return <div className="list-container">This is the list</div>;
+}
+
+//to finish this component
+// connect data from Redux store
+// use map state to props and connect function
