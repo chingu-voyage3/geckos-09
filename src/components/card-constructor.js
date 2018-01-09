a7194fa9c6c64fae64d7a3ebd4e56e6a67642591
Jordan Leslie <this.codedev@gmail.com>
2018-01-09 17:36:43 +0900

    Add card constructor modal and button to list header

---
+++ src/components/card-constructor.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import RaisedButton from "material-ui/RaisedButton";
+
+class CardConstructor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div className="card-constructor">
+        <RaisedButton label="Add a task" onClick={this.handleOpen} />
+        <Dialog
+          title="Make a card"
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          <p>
+            The form goes here
+          </p>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default CardConstructor;
--- src/components/list.js
+++ src/components/list.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import CardConstructor from "./card-constructor";
 import css from "../style/component.css";
 
-export default function List(props) {
-  return <div className="list-wrapper">
-  <div className="list-content">
-  <div className="list-header">Header</div>
-  <div className="list-cards">List cards go here</div>
-  </div>
-  </div>;
+class List extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="list-wrapper">
+        <div className="list-content">
+          <div className="list-header">
+            <h4>Header</h4>
+            <a className="open-card-constructor"><CardConstructor /></a>
+            <a className="list-menu">...</a>
+          </div>
+          <div className="list-cards">List cards go here</div>
+          {/* <a className="card-constructor">Add a card</a> */}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default List;
 
 //to finish this component
 // connect data from Redux store
