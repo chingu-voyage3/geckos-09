f8d2f8e20ae64fc3aa6bc826e2c15b030c547c46
Jordan Leslie <this.codedev@gmail.com>
2018-01-14 12:13:15 +0900

    Point firebase import statement to root config

--- src/reducers/reducer_lists.js
+++ src/reducers/reducer_lists.js
@@ -1,13 +1,7 @@
 import { lists } from "../settings/dummy-data";
-<<<<<<< HEAD
-import { GET_LIST } from "../constants/constants";
-
-export default function(state = lists, action) {
-=======
 import { GET_LISTS } from "../constants/constants";
 
 export default function(state = null, action) {
->>>>>>> 0bfa6a0028c187fef0dd228d3aaa922f02224e37
   switch (action.type) {
     case GET_LISTS:
       // Using the es6 spread operator to concat
