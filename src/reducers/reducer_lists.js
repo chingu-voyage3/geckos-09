f29057ca3820e37b83aa0a39398e10a1470e0dd8
Jordan Leslie <this.codedev@gmail.com>
2018-01-14 11:57:01 +0900

    Add static page design and list functionality

--- src/reducers/reducer_lists.js
+++ src/reducers/reducer_lists.js
@@ -1,7 +1,7 @@
-import { lists } from '../settings/dummy-data';
-import { GET_LIST } from '../constants/constants';
+import { lists } from "../settings/dummy-data";
+import { GET_LIST } from "../constants/constants";
 
-export default function (state = lists, action) {
+export default function(state = lists, action) {
   switch (action.type) {
     case GET_LIST:
       return action.payload;
