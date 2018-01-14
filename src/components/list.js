64bdbfb4f029bc1e1747ae9a7bf30e66213ecd23
Jordan Leslie <this.codedev@gmail.com>
2018-01-14 10:28:18 +0900

    Change list bg color to grey

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Card, CardActions, CardHeader, CardText } from "material-ui/Card";
+import FlatButton from "material-ui/FlatButton";
 import CardConstructor from "./card-constructor";
-import Card from "./card";
+import TaskCard from "./card";
 import css from "../style/component.css";
 
 class List extends Component {
@@ -11,7 +13,22 @@ class List extends Component {
   render() {
     return (
       <div className="list-wrapper">
-        <div className="list-content">
+        <Card
+          // Inline styling overrides class styling with material UI
+          style={{
+            backgroundColor: "#e2e4e6"
+          }}
+        >
+          <CardHeader title={this.props.text.header} />
+          <CardText expandable={false}>
+            <TaskCard />
+          </CardText>
+          <CardActions>
+            <FlatButton label="Action1" />
+            <FlatButton label="Action2" />
+          </CardActions>
+        </Card>
+        {/* <div className="list-content">
           <div className="list-header">
             <p>{this.props.text.header}</p>
             <a className="open-card-constructor">
@@ -21,9 +38,9 @@ class List extends Component {
           </div>
           <div className="list-cards">
             <Card />
-          </div>
-          {/* <a className="card-constructor">Add a card</a> */}
-        </div>
+          </div> */}
+        {/* <a className="card-constructor">Add a card</a> */}
+        {/* </div> */}
       </div>
     );
   }
