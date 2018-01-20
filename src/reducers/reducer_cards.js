8971917eac42d7bb040f7fec106c0c1ca4d30f9f
Jordan Leslie <this.codedev@gmail.com>
2018-01-20 10:18:18 -0600

    Card ids are added to an allId array in redux state

--- src/reducers/reducer_cards.js
+++ src/reducers/reducer_cards.js
@@ -15,13 +15,15 @@ const allId = (state, action) => {
   const { payload } = action;
   const { id } = payload;
   console.log(state);
-  return _.merge([], null, ...id);
+  if (state == null) {
+    return [id];
+  }
+  return [...state.allId, id];
 };
 export default function(state = null, action) {
   switch (action.type) {
     case ADD_CARD:
       return {
-        ...state,
         byListId: byListId(state, action),
         allId: allId(state, action)
       };
