0a25cec984eac111746676b79c7d27bafbcd9ea2
Jordan Leslie <this.codedev@gmail.com>
2018-01-22 13:35:16 -0600

    DELETE_CARD is a constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,5 +4,6 @@ export const ADD_LIST = "ADD_LIST";
 export const GET_CARDS = "GET_CARDS";
 export const GET_BOARD = "GET_BOARD";
 export const ADD_CARD = "ADD_CARD";
+export const DELETE_CARD = "DELETE_CARD";
 
 export const DUMMY_DATA = "DUMMY_DATA";
